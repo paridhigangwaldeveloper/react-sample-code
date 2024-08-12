ad9643adc5de2403e1d231d71e808bdd5138ec72
Paridhi <paridhigangwaldeveloper@gmail.com>
2024-08-12 19:16:16 +0530

    changes in index file

--- src/components/invoiceList/index.tsx
+++ src/components/invoiceList/index.tsx
@@ -45,7 +45,7 @@ function InvoiceList({
     setItemToDelete("");
   };
 console.log("object")
-console.log("object")
+
   const handleConfirm = async () => {
     await deleteInvoice(itemToDelete);
     handleClose();
